bbd236dbd0df7d5f350fab8c84c7a262e337d735
mstharindu <supuntharindu21@gmail.com>
2021-09-15 23:20:37 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return <MarketingApp />;
+};
