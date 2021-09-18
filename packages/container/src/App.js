de56be27a59cd399de247e7a717c95ffed95d012
mstharindu <supuntharindu21@gmail.com>
2021-09-18 14:35:04 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
-
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
